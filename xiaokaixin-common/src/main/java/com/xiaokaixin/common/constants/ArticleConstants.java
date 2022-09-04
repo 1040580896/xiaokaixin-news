232cd4b203b11d5e26647af1e7d1dacd05094280
xiaokaixin <1040580896@qq.com>
2022-09-04 20:21:40 +0800

    复习了freemarker的基本语法和使用以及静态化

--- xiaokaixin-common/src/main/java/com/xiaokaixin/common/constants/ArticleConstants.java
+++ xiaokaixin-common/src/main/java/com/xiaokaixin/common/constants/ArticleConstants.java
@@ -1,8 +1,11 @@
 package com.xiaokaixin.common.constants;
 
 public class ArticleConstants {
+    // 加载更多
     public static final Short LOADTYPE_LOAD_MORE = 1;
+    // 加载最新
     public static final Short LOADTYPE_LOAD_NEW = 2;
+    // 默认标签
     public static final String DEFAULT_TAG = "__all__";
 
 }
